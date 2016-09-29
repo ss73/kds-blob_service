3e882a95d5eff5d4b0c038a71d618627e6e95236
Stellan Söderström <stellan.soderstrom@gmail.com>
2016-09-29 11:36:38 +0200

    Added uuid generation of file names

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser')
+var UUID = require('uuid-1345');
 
 //app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/*+json' }))
@@ -12,27 +13,33 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views/info.html'));
 });
 
-app.get('/store', function(req, res) {
+app.get('/store', function (req, res) {
     res.sendFile(path.join(__dirname, 'views/store.html'));
-}); 
+});
 
-app.post('/store', function(req, res) {
+app.post('/store', function (req, res) {
     var json;
     console.log('Request type: ' + req.type);
-    if(req.type == 'application/x-www-form-urlencoded') {
+    if (req.type == 'application/x-www-form-urlencoded') {
         json = {
-            name : req.body.name,
-            content : req.body.content};
+            name: req.body.name,
+            content: req.body.content
+        };
     }
     else {
         json = req.body;
     }
     var name = json.name;
-    
+    UUID.v3({
+        namespace: UUID.blob_service,
+        name: name
+    }, function (err, result) {
+        console.log("Generated a name-based UUID using MD5:\n\t%s\n", result);
+    });
     res.send("OK\n");
 });
 
-app.get('/retrieve/:name', function(req, res){
+app.get('/retrieve/:name', function (req, res) {
 });
 
 app.listen(3000, function () {
